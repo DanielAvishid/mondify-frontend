c8a8478f185f49256c917a08b2812b1593c6cf38
Daniel Avishid <danielavishid@gmail.com>
2023-10-26 03:29:24 +0300

    F/AppSideBar/refactor the jsx of the cmp

--- src/cmps/AppSidebar.jsx
+++ src/cmps/AppSidebar.jsx
@@ -28,77 +28,98 @@ export function AppSidebar({ boards, onSaveBoard, onDuplicateBoard, onRemoveBoar
 
     console.log(boards);
     return (
-        <section className="app-sidebar flex column">
+        <section className="app-sidebar">
             <button className="close-btn" onClick={toggleSidebar}>
                 <Icon className="icon" icon={showSidebar ? NavigationChevronLeft : NavigationChevronRight} />
             </button>
-            <section className={`flex column main-sidebar ${showSidebar ? '' : 'close'}`}>
+            <div className="top-container">
                 <Button leftIcon={Home} kind="tertiary" className="home btn">Home</Button>
-                <Button leftIcon={MyWeek} kind="tertiary" className="my-week btn">My Week</Button>
-
-                <article className="flex align-center justify-between main-sec">
-                    <Button kind="tertiary" rightIcon={DropdownChevronDown}>
-                        <span></span>Main workspace
-                    </Button>
-                    <MenuButton>
-                        <Menu id="menu" size="large">
-                            <MenuItem icon={Add} title="Add new Workspace" />
-                            <MenuItem icon={Home} title="Add new Workspace" />
-                            <MenuItem icon={Home} title="Add new Workspace" />
-                        </Menu>
-                    </MenuButton>
-
-                </article>
-                <article className="flex align-center justify-between search-sec">
-                    <Search placeholder="Search" iconName={Filter} className="search" />
-                    <span><Button
-                        size="md"
-                        onClick={onAddBoard}>
-                        <Add />
-                    </Button></span>
-                </article>
-                <Button leftIcon={Gantt} kind="tertiary" className="test_board btn">Test_board</Button>
-
-                <DragDropContext onDragEnd={handleOnDragEnd}>
-                    <Droppable droppableId="board" type="group">
-                        {(provided) => (
-                            <div {...provided.droppableProps} ref={provided.innerRef}>
-                                {boards.length > 0 && boards.map((board, index) => (
-                                    <Draggable draggableId={board._id} index={index} key={board._id}>
-                                        {(provided) => (
-                                            <article
-                                                className="flex align-center justify-between board-label"
-                                                {...provided.dragHandleProps}
-                                                {...provided.draggableProps}
-                                                ref={provided.innerRef}
-                                            >
-                                                <Button
-                                                    key={board._id}
-                                                    onClick={() => navigate(`/board/${board._id}`)}
-                                                    leftIcon={Board}
-                                                    kind="tertiary"
-                                                    className="board btn">
-                                                    {board.title}
-                                                </Button>
-                                                <MenuButton className="board-menu">
-                                                    <Menu id="menu" size="large">
-                                                        <MenuItem icon={Duplicate} title="Duplicate Board" onClick={() => onDuplicateBoard({ boardId: board._id })} />
-                                                        <MenuItem icon={Delete} title="Delete" onClick={() => onRemoveBoard({ boardId: board._id })} />
-                                                    </Menu>
-                                                </MenuButton>
-                                            </article>
-                                        )}
-                                    </Draggable>
-                                ))}
-                                {provided.placeholder}
-                            </div>
-                        )}
-                    </Droppable>
-                </DragDropContext>
-            </section>
-        </section >
+                <Button leftIcon={MyWeek} kind="tertiary" className="my-week btn">My work</Button>
+            </div>
+            <div className="bottom-container">
+                <div className="workspace-container">
+                    <div className="workspace-contoller">
+                        <Button className="workspace-btn" kind="tertiary" rightIcon={DropdownChevronDown}>
+                            <span>Main workspace</span>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
+//     return (
+//         <section className="app-sidebar flex column">
+//             <button className="close-btn" onClick={toggleSidebar}>
+//                 <Icon className="icon" icon={showSidebar ? NavigationChevronLeft : NavigationChevronRight} />
+//             </button>
+//             <section className={`flex column main-sidebar ${showSidebar ? '' : 'close'}`}>
+//                 <Button leftIcon={Home} kind="tertiary" className="home btn">Home</Button>
+//                 <Button leftIcon={MyWeek} kind="tertiary" className="my-week btn">My Week</Button>
+
+//                 <article className="flex align-center justify-between main-sec">
+//                     <Button kind="tertiary" rightIcon={DropdownChevronDown}>
+//                         <span></span>Main workspace
+//                     </Button>
+//                     <MenuButton>
+//                         <Menu id="menu" size="large">
+//                             <MenuItem icon={Add} title="Add new Workspace" />
+//                             <MenuItem icon={Home} title="Add new Workspace" />
+//                             <MenuItem icon={Home} title="Add new Workspace" />
+//                         </Menu>
+//                     </MenuButton>
+
+//                 </article>
+//                 <article className="flex align-center justify-between search-sec">
+//                     <Search placeholder="Search" iconName={Filter} className="search" />
+//                     <span><Button
+//                         size="md"
+//                         onClick={onAddBoard}>
+//                         <Add />
+//                     </Button></span>
+//                 </article>
+//                 <Button leftIcon={Gantt} kind="tertiary" className="test_board btn">Test_board</Button>
+
+//                 <DragDropContext onDragEnd={handleOnDragEnd}>
+//                     <Droppable droppableId="board" type="group">
+//                         {(provided) => (
+//                             <div {...provided.droppableProps} ref={provided.innerRef}>
+//                                 {boards.length > 0 && boards.map((board, index) => (
+//                                     <Draggable draggableId={board._id} index={index} key={board._id}>
+//                                         {(provided) => (
+//                                             <article
+//                                                 className="flex align-center justify-between board-label"
+//                                                 {...provided.dragHandleProps}
+//                                                 {...provided.draggableProps}
+//                                                 ref={provided.innerRef}
+//                                             >
+//                                                 <Button
+//                                                     key={board._id}
+//                                                     onClick={() => navigate(`/board/${board._id}`)}
+//                                                     leftIcon={Board}
+//                                                     kind="tertiary"
+//                                                     className="board btn">
+//                                                     {board.title}
+//                                                 </Button>
+//                                                 <MenuButton className="board-menu">
+//                                                     <Menu id="menu" size="large">
+//                                                         <MenuItem icon={Duplicate} title="Duplicate Board" onClick={() => onDuplicateBoard({ boardId: board._id })} />
+//                                                         <MenuItem icon={Delete} title="Delete" onClick={() => onRemoveBoard({ boardId: board._id })} />
+//                                                     </Menu>
+//                                                 </MenuButton>
+//                                             </article>
+//                                         )}
+//                                     </Draggable>
+//                                 ))}
+//                                 {provided.placeholder}
+//                             </div>
+//                         )}
+//                     </Droppable>
+//                 </DragDropContext>
+//             </section>
+//         </section >
+//     )
+// }
 
 
 
