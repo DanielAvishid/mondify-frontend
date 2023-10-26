9055a6833d98050508cdb627b5adb5072b3c1d99
Ofir Kaspi <kaspiofir@gmail.com>
2023-10-26 19:56:37 +0300

    F/memebes-crud/modal layout fixed

--- src/cmps/dynamicCmps/Members.jsx
+++ src/cmps/dynamicCmps/Members.jsx
@@ -1,4 +1,4 @@
-import { Icon, Search } from "monday-ui-react-core"
+import { Box, Icon, Search } from "monday-ui-react-core"
 import { Invite, Close } from "/node_modules/monday-ui-react-core/src/components/Icon/Icons"
 import { useState } from "react"
 
@@ -28,65 +28,70 @@ export function Members({ info, board }) {
             )}
 
             {isModalOpen &&
-                <div className="members-modal">
-                    {membersIds.length > 0 && (
-                        <section className="participate-members flex">
-                            {membersIds.map((memberId) => {
-                                const matchingMember = board.members.find(member => member._id === memberId);
-                                return (
-                                    <div className="member-container flex align-center" key={memberId}>
-                                        <div className="member-details flex align-center">
-                                            <img
-                                                key={matchingMember._id}
-                                                className="avatar"
-                                                src={matchingMember.imgUrl}
-                                            />
-                                            <span className="member-name">{matchingMember.fullname}</span>
-                                        </div>
-                                        <div className="remove-member flex align-center justify-center">
-                                            <Icon
-                                                icon={Close}
-                                                iconSize={8}
-                                            />
-                                        </div>
-                                    </div>
-                                )
-                            })}
-                        </section>
-                    )}
-                    <section className="member-picker-container">
-                        <div className="member-list">
-                            <Search
-                                size={Search.sizes.SMALL}
-                                placeholder="Search names, roles to teams"
-                                wrapperClassName="monday-storybook-search_size"
-                            />
-                            <div className="suggest-container"><span className="suggest-title">Suggested people</span></div>
-                            {modalMembers.map((member) => (
-                                <div className="member-row flex align-center" key={member._id}>
-                                    <img
-                                        key={member._id}
-                                        className="avatar grid align-center justify-center"
-                                        src={member.imgUrl}
-                                    />
-                                    <span className="member-name">{member.fullname}</span>
-                                </div>
-                            ))}
-                            <div className="member-row flex align-center">
-                                <div className="invite-icon-container grid align-center justify-center">
-                                    <Icon
-                                        // customColor={group.style.backgroundColor}
-                                        // className="invite-icon"
-                                        icon={Invite}
-                                        iconSize={16}
-                                    />
+                <>
+                    <div className="pointer"></div>
+                    <div className="members-modal">
 
+                        {membersIds.length > 0 && (
+                            <section className="participate-members flex">
+                                {membersIds.map((memberId) => {
+                                    const matchingMember = board.members.find(member => member._id === memberId);
+                                    return (
+                                        <div className="member-container flex align-center" key={memberId}>
+                                            <div className="member-details flex align-center">
+                                                <img
+                                                    key={matchingMember._id}
+                                                    className="avatar"
+                                                    src={matchingMember.imgUrl}
+                                                />
+                                                <span className="member-name">{matchingMember.fullname}</span>
+                                            </div>
+                                            <div className="remove-member flex align-center justify-center">
+                                                <Icon
+                                                    icon={Close}
+                                                    iconSize={8}
+                                                />
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                            </section>
+                        )}
+                        <section className="member-picker-container">
+                            <div className="member-list">
+                                <Search
+                                    size={Search.sizes.SMALL}
+                                    placeholder="Search names, roles to teams"
+                                    wrapperClassName="monday-storybook-search_size"
+                                />
+                                <div className="suggest-container"><span className="suggest-title">Suggested people</span></div>
+                                {modalMembers.map((member) => (
+                                    <div className="member-row flex align-center" key={member._id}>
+                                        <img
+                                            key={member._id}
+                                            className="avatar grid align-center justify-center"
+                                            src={member.imgUrl}
+                                        />
+                                        <span className="member-name">{member.fullname}</span>
+                                    </div>
+                                ))}
+                                <div className="member-row flex align-center">
+                                    <div className="invite-icon-container grid align-center justify-center">
+                                        <Icon
+                                            // customColor={group.style.backgroundColor}
+                                            // className="invite-icon"
+                                            icon={Invite}
+                                            iconSize={16}
+                                        />
+
+                                    </div>
+                                    <span className="member-name">Invite a new member by email</span>
                                 </div>
-                                <span className="member-name">Invite a new member by email</span>
                             </div>
-                        </div>
-                    </section>
-                </div>
+                        </section>
+                    </div>
+                </>
+
             }
         </td >
     )
