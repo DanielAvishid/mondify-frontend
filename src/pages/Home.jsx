34a5ec9cfec8f639ccc6541bbdae2c1acbb02352
Daniel Avishid <danielavishid@gmail.com>
2023-10-31 23:28:41 +0200

    F/Home/class names

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,36 @@
-import { Icon } from "monday-ui-react-core"
+import { Icon, Link } from "monday-ui-react-core"
 import { useNavigate } from "react-router-dom"
 import { MoveArrowRight } from "/node_modules/monday-ui-react-core/src/components/Icon/Icons"
 import imgUrl from '../assets/img/monday-img.png'
 import { useState } from "react"
 
-
-
 export function Home() {
 
     const navigate = useNavigate()
-
+    https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/monday-logo-x2.png
     return (
         <section className="home-page">
-            <header className="flex justify-between align-center">
-                <div>Logo</div>
-                <div className="flex align-center">
-                    <div>
+            <header className="header">
+                <div className="logo">
+                    <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/monday-logo-x2.png" alt="" />
+                    <span>
+                        mondify
+                    </span>
+                </div>
+                <div className="get-started">
+                    <div className="login">
                         <a href="/board">Log in</a>
                     </div>
-                    <button className="flex align-center justify-center" onClick={() => navigate('/board/#')}>
+                    <button className="btn">
                         <span>Get Started</span>
                         <Icon icon={MoveArrowRight} iconSize="14" />
                     </button>
                 </div>
             </header>
-            <div className="first-container flex justify-center align-center column">
-                <div className="flex justify-center align-center column">
-                    <h1>
-                        <span>A platform built for a <br /> new way of working</span>
+            <div className="hero">
+                <div className="content">
+                    <h1 className="big">
+                        <span className="">A platform built for a <br /> new way of working</span>
                     </h1>
                     <h2>What would you like to manage with monday.com Work OS?</h2>
                 </div>
