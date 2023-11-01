38d7cd8bb58376d0d9e6e193ef8d6c8fc8e0bf98
Daniel Avishid <danielavishid@gmail.com>
2023-11-01 04:18:34 +0200

    F/Home/header, hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,6 @@ import { useState } from "react"
 export function Home() {
 
     const navigate = useNavigate()
-    https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/remote_mondaycom_static/img/monday-logo-x2.png
     return (
         <section className="home-page">
             <div className="header-container">
@@ -22,7 +21,7 @@ export function Home() {
                         {/* <div className="login">
                         <a href="/board">Log in</a>
                     </div> */}
-                        <button className="btn">
+                        <button className="start-btn">
                             <span className="span">Get Started</span>
                             <Icon className="icon" icon={MoveArrowRight} iconSize="14" />
                         </button>
@@ -42,75 +41,73 @@ export function Home() {
                     <div className="cards-container">
                         <div className="card">
                             <div className="checkbox"></div>
-                            <div className="cluster-img">
-                                <img src='https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured images/Home Page - 2022 Rebrand/first_fold/icons/design_icon.png' />
-                            </div>
-                            {/* <p>Sales & CRM</p> */}
-                        </div>
-                        {/* <div className="selectable-cluster flex align-center column">
-                            <div className="checkbox"></div>
-                            <div className="cluster-img">
-                                <img src={imgUrl} />
-                            </div>
+                            <img className="cluster-img" src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured images/Home Page - 2022 Rebrand/first_fold/icons/design_icon.png" />
                             <p>Creative & design</p>
                         </div>
-                        <div className="selectable-cluster flex align-center column">
+                        <div className="card">
                             <div className="checkbox"></div>
                             <div className="cluster-img">
-                                <img src={imgUrl} />
+                                <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured images/Home Page - 2022 Rebrand/first_fold/icons/dev_icon.png" />
                             </div>
                             <p>Software development</p>
                         </div>
-                        <div className="selectable-cluster flex align-center column">
+                        <div className="card">
                             <div className="checkbox"></div>
                             <div className="cluster-img">
-                                <img src={imgUrl} />
-                            </div>
-                            <p>Project management</p>
-                        </div>
-                        <div className="selectable-cluster flex align-center column">
-                            <div className="checkbox"></div>
-                            <div className="cluster-img">
-                                <img src={imgUrl} />
-                            </div>
-                            <p>Task management</p>
-                        </div>
-                        <div className="selectable-cluster flex align-center column">
-                            <div className="checkbox"></div>
-                            <div className="cluster-img">
-                                <img src={imgUrl} />
+                                <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured images/Home Page - 2022 Rebrand/first_fold/icons/marketing_icon.png" />
                             </div>
                             <p>Marketing</p>
                         </div>
-                        <div className="selectable-cluster flex align-center column">
+                        <div className="card">
                             <div className="checkbox"></div>
                             <div className="cluster-img">
-                                <img src={imgUrl} />
+                                <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured images/Home Page - 2022 Rebrand/first_fold/icons/pmo_icon.png" />
+                            </div>
+                            <p>Project managemnet</p>
+                        </div>
+                        <div className="card">
+                            <div className="checkbox"></div>
+                            <div className="cluster-img">
+                                <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured images/Home Page - 2022 Rebrand/first_fold/icons/crm_icon.png" />
+                            </div>
+                            <p>Sales & CRM</p>
+                        </div>
+                        <div className="card">
+                            <div className="checkbox"></div>
+                            <div className="cluster-img">
+                                <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured images/Home Page - 2022 Rebrand/first_fold/icons/task_icon.png" />
+                            </div>
+                            <p>Task management</p>
+                        </div>
+                        <div className="card">
+                            <div className="checkbox"></div>
+                            <div className="cluster-img">
+                                <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured images/Home Page - 2022 Rebrand/first_fold/icons/hr_icon.png" />
                             </div>
                             <p>HR</p>
                         </div>
-                        <div className="selectable-cluster flex align-center column">
+                        <div className="card">
                             <div className="checkbox"></div>
                             <div className="cluster-img">
-                                <img src={imgUrl} />
+                                <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured images/Home Page - 2022 Rebrand/first_fold/icons/operations_icon.png" />
                             </div>
                             <p>Operations</p>
                         </div>
-                        <div className="selectable-cluster flex align-center column">
+                        <div className="card">
                             <div className="checkbox"></div>
                             <div className="cluster-img">
-                                <img src={imgUrl} />
+                                <img src="https://dapulse-res.cloudinary.com/image/upload/f_auto,q_auto/Generator_featured images/Home Page - 2022 Rebrand/first_fold/icons/workflows_icon.png" />
                             </div>
                             <p>More workflows</p>
-                        </div> */}
+                        </div>
                     </div>
-                    {/* <div>
-                        <button className="flex align-center justify-center" onClick={() => navigate('/board/#')}>
-                            <span>Get Started</span>
-                            <Icon icon={MoveArrowRight} iconSize="18" />
+                    <div className="btn-container">
+                        <button className="start-btn" onClick={() => navigate('/board/#')}>
+                            <span className="span">Get Started</span>
+                            <Icon className="icon" icon={MoveArrowRight} iconSize="18" />
                         </button>
                     </div>
-                    <p className="small-words">No credit card needed   ✦    Unlimited time on Free plan</p> */}
+                    <p className="small-words">No credit card needed   ✦    Unlimited time on Free plan</p>
                 </div>
 
             </div>
