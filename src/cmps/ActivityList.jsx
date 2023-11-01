cc3ab462e49e462cf4811417c1e265e4713a1b95
Ofir Kaspi <kaspiofir@gmail.com>
2023-11-01 14:39:39 +0200

    F/ActivityPreview/ before changing to grid layout

--- src/cmps/ActivityList.jsx
+++ src/cmps/ActivityList.jsx
@@ -4,7 +4,7 @@ export function ActivityList({ board }) {
     return (
         <section className="activity-list">
             {board.activities.map(activity => (
-                <ActivityPreview activity={activity} />
+                <ActivityPreview board={board} activity={activity} />
             ))}
         </section>
     )
--- src/cmps/ActivityPreview.jsx
+++ src/cmps/ActivityPreview.jsx
@@ -2,9 +2,28 @@ import { utilService } from "../services/util.service";
 import { Time, NavigationChevronRight } from "/node_modules/monday-ui-react-core/src/components/Icon/Icons"
 import { Avatar, Icon } from "monday-ui-react-core";
 
-export function ActivityPreview({ activity }) {
+export function ActivityPreview({ board, activity }) {
     console.log(activity);
     const time = utilService.getTimePassed(activity.timestamp)
+
+    let place;
+    if (activity.location.task) {
+        for (const group of board.groups) {
+            const task = group.tasks.find((task) => task.title);
+            if (task) {
+                place = task.title;
+                break; // Stop searching after finding the first task with a title
+            }
+        }
+    } else if (activity.location.group) {
+        // Find the group by groupId
+        const group = board.groups.find((group) => group.id === activity.location.group);
+        if (group) {
+            place = group.title;
+        }
+    } else {
+        place = board.title;
+    }
 
     return (
         <div className="activity-preview flex align-center">
@@ -18,7 +37,7 @@ export function ActivityPreview({ activity }) {
                     src={"https://style.monday.com/static/media/person1.de30c8ee.png"}
                     className="avatar"
                 />
-                <span>Place</span>
+                <span className="ellipsis-text place-span">{place}</span>
             </div>
             <div className="activity-change flex align-center">
                 <div className="change-type flex">
@@ -26,9 +45,9 @@ export function ActivityPreview({ activity }) {
                     <span>type</span>
                 </div>
                 <div className="change-values flex align-center">
-                    <span className="prev-val flex justify-center">before</span>
+                    <span className="prev-val flex justify-center">{activity.prevValue}</span>
                     <Icon icon={NavigationChevronRight} className="change-icon" />
-                    <span className="new-val flex justify-center">after</span>
+                    <span className="new-val flex justify-center">{activity.newValue}</span>
                 </div>
             </div>
         </div>
