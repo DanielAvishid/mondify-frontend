07c0e0d9d20468bc9195c0e4d30e4db4638aa2fc
Daniel Avishid <danielavishid@gmail.com>
2023-11-05 04:28:35 +0200

    MemebersSummary if no memebers in group show span

--- src/cmps/dynamicSummaryCmps/MembersSummary.jsx
+++ src/cmps/dynamicSummaryCmps/MembersSummary.jsx
@@ -10,11 +10,11 @@ export function MembersSummary({ board, group }) {
     const membersInfo = uniqueGroupMemberIds.map(memberId => {
         const member = boardMembers.find(boardMember => boardMember._id === memberId);
         return member || {}; // Ensure a member object, even if not found
-    });
+    })
 
     return (
         <div className="members-summary flex align-center justify-center">
-            <AvatarGroupCmp members={membersInfo} />
+            {membersInfo.length > 0 ? <AvatarGroupCmp members={membersInfo} /> : <span></span>}
         </div >
     )
 }
