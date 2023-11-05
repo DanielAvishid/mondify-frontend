05700c10d7059e522bde9fb094b6d78a2230a916
Ofir Kaspi <kaspiofir@gmail.com>
2023-11-05 15:47:22 +0200

    F/Dashboard/made basic layout and one soughnut chart as an example with static data.

--- src/cmps/Dashboard.jsx
+++ src/cmps/Dashboard.jsx
@@ -1,5 +1,54 @@
+import {
+    Chart as ChartJS,
+    ArcElement,
+    Tooltip,
+    Legend
+} from 'chart.js'
+
+import { Doughnut } from 'react-chartjs-2'
+
+ChartJS.register(
+    ArcElement,
+    Tooltip,
+    Legend
+)
+
 export function Dashboard() {
+
+    const data = {
+        labels: ['Yes', 'No'],
+        datasets: [{
+            label: 'Poll',
+            data: [3, 6],
+            backgrounfColor: ['black', 'red'],
+            borderColor: ['black', 'red'],
+            circumference: 180,
+            rotation: 270
+        }]
+    }
+
+    const options = {
+
+    }
+
+
+
     return (
-        <div className="dashboard">hola</div>
+        <div className="dashboard full">
+            <div className='overall-progress-container'>
+                <div>mapping here through groups and showing id done or show a precentage</div>
+            </div>
+            <div className='members-chart-container'>members</div>
+            <div className='date-counter-container'>date</div>
+            <div className='doughnut-container'>
+                <h1>Dashboard</h1>
+                <div>
+                    <Doughnut
+                        data={data}
+                        options={options}
+                    ></Doughnut>
+                </div>
+            </div>
+        </div>
     )
 }
