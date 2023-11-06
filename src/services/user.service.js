d922e934dc7d37a4bd3903494c2477b54c86a8bf
Daniel Avishid <danielavishid@gmail.com>
2023-11-06 15:28:08 +0200

    move to http service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,6 +2,8 @@ import { storageService } from './async-storage.service'
 import { httpService } from './http.service'
 
 const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
+const USER_URL = 'user/'
+const AUTH_URL = 'auth/'
 
 export const userService = {
     login,
@@ -9,7 +11,6 @@ export const userService = {
     signup,
     getLoggedinUser,
     getDefaultUser,
-    saveLocalUser,
     getUsers,
     getById,
     remove,
@@ -19,61 +20,90 @@ export const userService = {
 window.userService = userService
 
 function getUsers() {
-    return storageService.query('user')
-    // return httpService.get(`user`)
-}
-
-async function getById(userId) {
-    const user = await storageService.get('user', userId)
-    // const user = await httpService.get(`user/${userId}`)
-    return user
-}
-
-function remove(userId) {
-    return storageService.remove('user', userId)
-    // return httpService.delete(`user/${userId}`)
-}
-
-async function update({ _id, score }) {
-    const user = await storageService.get('user', _id)
-    await storageService.put('user', user)
-
-    // const user = await httpService.put(`user/${_id}`, {_id, score})
-    // // Handle case in which admin updates other user's details
-    if (getLoggedinUser()._id === user._id) saveLocalUser(user)
-    return user
-}
-
-async function login(userCred) {
-    const users = await storageService.query('user')
-    const user = users.find(user => user.username === userCred.username)
-    // const user = await httpService.post('auth/login', userCred)
-    if (user) {
-        return saveLocalUser(user)
+    // return storageService.query('user')
+    try {
+        return httpService.get(USER_URL)
+    } catch (err) {
+        throw err
     }
 }
 
-async function signup(userCred) {
-    console.log(userCred)
-    if (!userCred.imgUrl) userCred.imgUrl = 'https://style.monday.com/static/media/person2.24c7233e.png'
-    const user = await storageService.post('user', userCred)
-    // const user = await httpService.post('auth/signup', userCred)
-    return saveLocalUser(user)
+async function getById(userId) {
+    try {
+        const user = await httpService.get(USER_URL + userId)
+        return user
+    } catch (err) {
+        throw err
+    }
+    // const user = await storageService.get('user', userId)
+    // return user
+}
+
+function remove(userId) {
+    try {
+        return httpService.delete(USER_URL + userId)
+    } catch (err) {
+        throw err
+    }
+    // return storageService.remove('user', userId)
+}
+
+async function update(user) {
+    if (getLoggedinUser()._id === user._id) {
+        try {
+            return await httpService.put(USER_URL + user._id, user)
+        } catch (err) {
+            throw err
+        }
+    }
+
+    // const user = await storageService.get('user', _id)
+    // await storageService.put('user', user)
+
+    // if (getLoggedinUser()._id === user._id) saveLocalUser(user)
+    // return user
+}
+
+async function login({ username, password }) {
+    try {
+        const user = await httpService.post(AUTH_URL + 'login', { username, password })
+        if (user) return _setLoggedinUser(user)
+    } catch (err) {
+        throw err
+    }
+    // const users = await storageService.query('user')
+    // const user = users.find(user => user.username === userCred.username)
+    // if (user) {
+    //     return saveLocalUser(user)
+    // }
+}
+
+async function signup({ username, password, fullname, imgUrl = 'https://cdn1.monday.com/dapulse_default_photo.png' }) {
+    try {
+        const userToSign = { username, password, fullname, imgUrl }
+        const user = await httpService.post(AUTH_URL + 'signup', userToSign)
+        if (user) return _setLoggedinUser(user)
+    } catch (err) {
+        throw err
+    }
+    // const user = await storageService.post('user', userCred)
+    return _setLoggedinUser(user)
 }
 
 async function logout() {
     sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
+    return Promise.resolve()
     // return await httpService.post('auth/logout')
-}
-
-function saveLocalUser(user) {
-    user = { _id: user._id, fullname: user.fullname, imgUrl: user.imgUrl }
-    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
-    return user
 }
 
 function getLoggedinUser() {
     return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER))
+}
+
+function _setLoggedinUser(user) {
+    const userToSave = { _id: user._id, fullname: user.fullname, imgUrl: user.imgUrl }
+    sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(userToSave))
+    return userToSave
 }
 
 function getDefaultUser() {
