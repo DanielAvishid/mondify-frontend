9e59243d072f279b58eb0434eefe3895d1350659
Ofir Kaspi <kaspiofir@gmail.com>
2023-11-07 02:32:20 +0200

    F/MembersFilterModal/made it lock perfect

--- src/cmps/MembersFilterModal.jsx
+++ src/cmps/MembersFilterModal.jsx
@@ -3,6 +3,9 @@ import { utilService } from '../services/util.service';
 
 export function MembersFilterModal({ members, setFilterBy, filterBy }) {
 
+    const filteredPersonId = members.find(member => member._id === filterBy.person)?._id || '';
+
+
     function onSetFilterBy(memberId) {
         console.log('memberId', memberId);
         setFilterBy((prevFilterBy) => ({
@@ -17,40 +20,30 @@ export function MembersFilterModal({ members, setFilterBy, filterBy }) {
             <p className="second-title">Filter items and subitems by person</p>
 
             <div className="member-list-container">
-                {members.map(member => {
-                    let dynSelectedPersonClass = ''
-
-                    if (filterBy.person) {
-                        dynSelectedPersonClass = filterBy.person._id === member._id ? 'selected' : '';
-                    }
-
-                    return (
-                        <div className='person-img-container' key={member._id}>
-                            {(!member.imgUrl) ? (
-                                <div className={'test ' + dynSelectedPersonClass}>
-                                    <Avatar
-                                        className='avatar-img'
-                                        size={Avatar.sizes.SMALL}
-                                        type={Avatar.types.TEXT}
-                                        text={utilService.getNameInitials(member.fullname)}
-                                        backgroundColor={Avatar.colors.BLACKISH}
-                                        ariaLabel={member.fullname}
-                                        onClick={() => onSetFilterBy(member)}
-                                    />
-                                </div>
-                            ) : (
-                                <Avatar
-                                    className={'avatar-img ' + dynSelectedPersonClass}
-                                    ariaLabel={member.fullname}
-                                    size={Avatar.sizes.SMALL}
-                                    src={member.imgUrl}
-                                    type="img"
-                                    onClick={() => onSetFilterBy(member._id)}
-                                />
-                            )}
-                        </div>
-                    )
-                })}
+                {members.map(member => (
+                    <div className={`person-img-container ${filteredPersonId === member._id ? 'selected' : ''}`} key={member._id}>
+                        {(!member.imgUrl) ? (
+                            <Avatar
+                                className='avatar-img'
+                                size={Avatar.sizes.SMALL}
+                                type={Avatar.types.TEXT}
+                                text={utilService.getNameInitials(member.fullname)}
+                                backgroundColor={Avatar.colors.BLACKISH}
+                                ariaLabel={member.fullname}
+                                onClick={() => onSetFilterBy(member)}
+                            />
+                        ) : (
+                            <Avatar
+                                className='avatar-img'
+                                ariaLabel={member.fullname}
+                                size={Avatar.sizes.SMALL}
+                                src={member.imgUrl}
+                                type="img"
+                                onClick={() => onSetFilterBy(member._id)}
+                            />
+                        )}
+                    </div>
+                ))}
             </div>
         </div>
     )
