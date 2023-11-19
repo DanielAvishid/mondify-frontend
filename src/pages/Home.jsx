3e6a041398caff68934d937e38e441a88b12d264
Ofir Kaspi <kaspiofir@gmail.com>
2023-11-20 00:33:58 +0200

    F/Login/login page is now responsive for mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,26 +9,6 @@ import "react-multi-carousel/lib/styles.css";
 
 export function Home() {
 
-    const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
-    };
-
     const [isHeroBtnHover, setIsHeroBtnHover] = useState(false)
     const [isCardHover, setIsCardHover] = useState([false, false, false, false, false, false, false, false, false])
     const [isCardChecked, setIsCardChecked] = useState([false, false, false, false, false, false, false, false, false])
@@ -100,6 +80,26 @@ export function Home() {
         const newArray = [...isCardHover]
         setIsCardHover(newArray)
     }
+
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
 
     return (
         <section className="home-page">
